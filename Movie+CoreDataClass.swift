87ed036bff10021d9d92844733663a5c6bf56913
Kenichi Takahama <hammer528@msn.com>
2023-01-23 13:52:37 +0900

    project12_part1_2 finish

---
+++ Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by K.Takahama on R 5/01/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
