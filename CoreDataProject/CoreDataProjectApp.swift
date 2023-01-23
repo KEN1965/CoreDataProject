a7eaad484597896a256d99ccd3924438e21de2f4
Kenichi Takahama <hammer528@msn.com>
2023-01-23 13:24:59 +0900

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by K.Takahama on R 5/01/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
